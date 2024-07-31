c3e5b07c4a086f27526bd38210b25cea5c90baa4
Mariana F. Pereira <1231845@isep.ipp.pt>
2024-07-31 15:20:02 +0100

    [FEAT] #1 changed package of TitleHeading.css

--- fabamaq_users/src/components/Headings/TitleHeading.jsx
+++ fabamaq_users/src/components/Headings/TitleHeading.jsx
@@ -1,7 +1,7 @@
 import { UserCircle, ChatText } from "@phosphor-icons/react";
 import PropTypes from "prop-types";
 import { useEffect } from "react";
-import '../../styles/TitleHeading.css';
+import './TitleHeading.css';
 
 const TitleHeading = ({ title = "", subtitle = "", showUserIcon = false, showChatIcon = false }) => {
     useEffect(() => {
